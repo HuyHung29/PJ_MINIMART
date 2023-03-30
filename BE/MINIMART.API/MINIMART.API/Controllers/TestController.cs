70be002b0dc5c24f78d7b9afe7e252027b435cc5
HuyHung <lehuyhugn29@gmail.com>
2023-03-30 22:40:50 +0700

    Tạo test controller

---
+++ BE/MINIMART.API/MINIMART.API/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MINIMART.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
